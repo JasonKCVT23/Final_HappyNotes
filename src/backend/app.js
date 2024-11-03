f7466b609540d99af84bd8f99722b7bed608ea2f
TimChen1011 <110208059@g.nccu.edu.tw>
2024-11-03 15:14:51 +0800

    feat: add CORS middleware and update API routes for whiteboards and cards

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const whiteboardRoutes = require("./routes/whiteboardRoutes");
+const cardRoutes = require("./routes/cardRoutes");
+
 const app = express();
-const whiteboardRoutes = require('./routes/whiteboardRoutes');
+
+// cors middleware to allow cross-origin requests
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend server port
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use('/', whiteboardRoutes);
+app.use("/api/whiteboards", whiteboardRoutes);
+app.use("/api/cards", cardRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
